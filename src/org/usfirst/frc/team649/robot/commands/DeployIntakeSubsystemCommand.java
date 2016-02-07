6d1fdda00dd2af165a40d27fc00f0b28491b41cd
Allgamesdude <kedz07@gmail.com>
2016-02-07 12:02:48 -0800

    Added LaunchIntakeSubsystemCommand(untouched) and DeployIntakeSubsystemCommand, which is filled with everything except for the protected execute method.

--- src/org/usfirst/frc/team649/robot/commands/DeployIntakeSubsystemCommand.java
+++ src/org/usfirst/frc/team649/robot/commands/DeployIntakeSubsystemCommand.java
@@ -1,23 +1,34 @@
 package org.usfirst.frc.team649.robot.commands;
 
+import org.usfirst.frc.team649.robot.Robot;
+import org.usfirst.frc.team649.robot.RobotMap;
+import org.usfirst.frc.team649.robot.subsystems.IntakeSubsystem;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
-public class IntakeSubsystemCommand extends Command {
+public class DeployIntakeSubsystemCommand extends Command {
 
-	public IntakeSubsystemCommand() {
+	IntakeSubsystem intake = new IntakeSubsystem();
+
+	public DeployIntakeSubsystemCommand() {
 		// Use requires() here to declare subsystem dependencies
 		// eg. requires(chassis);
+		super("Deploy Intake using Double Solenoids and Rollers");
+		requires(intake);
 	}
 
 	// Called just before this Command runs the first time
 	protected void initialize() {
+		intake.setFwdRolSpd(0);
+		intake.setCenteringModuleSpeed(0);
 	}
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
+		
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
@@ -27,6 +38,9 @@ public class IntakeSubsystemCommand extends Command {
 
 	// Called once after isFinished returns true
 	protected void end() {
+		intake.setFwdRolSpd(0);
+		intake.setCenteringModuleSpeed(0);
+		
 	}
 
 	// Called when another command which requires one or more of the same
