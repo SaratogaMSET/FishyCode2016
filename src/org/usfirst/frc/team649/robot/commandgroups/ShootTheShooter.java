174b6734eb5ab937788de9bb10f4bbac36cad64f
Kabir Manghnani <kaju99@gmail.com>
2016-02-22 22:10:37 -0800

    some code

--- src/org/usfirst/frc/team649/robot/commandgroups/ShootTheShooter.java
+++ src/org/usfirst/frc/team649/robot/commandgroups/ShootTheShooter.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj.command.WaitCommand;
 public class ShootTheShooter extends CommandGroup {
 	public ShootTheShooter(){
 		addSequential(new ShooterSet(DoubleSolenoid.Value.kForward));
-		addSequential(new WaitCommand(1.0));
+		addSequential(new WaitCommand(0.5));
 		addSequential(new ShooterSet(DoubleSolenoid.Value.kReverse));
 	}
 }
