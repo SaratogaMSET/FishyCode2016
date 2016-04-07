fc1e7c3d60e3b9c2cf9670ee4471f5091aa88839
cabosanlucas <kaju99@gmail.com>
2016-04-07 11:28:00 -0700

    merged result

--- src/org/usfirst/frc/team649/robot/subsystems/CameraSubsystem.java
+++ src/org/usfirst/frc/team649/robot/subsystems/CameraSubsystem.java
@@ -41,8 +41,6 @@ public class CameraSubsystem extends Subsystem {
 	
 	public boolean noOpencvErrors;
 	
-	public static double CAM_UP_ANGLE = 5; //TODO
-	public static double CAM_DOWN_ANGLE = 110;
 	public static boolean CAM_UP = true;
 	
 	public static double WIDTH_TARGET = 18.5; //in
@@ -56,17 +54,8 @@ public class CameraSubsystem extends Subsystem {
 	public static double POS_3_CAM_X = 160; //pixels
 	public static double POS_4_CAM_X = 160; //pixels
 	public static double POS_5_CAM_X = 160; //pixels
-	/*
-	public static String image_1 = "/home/lvuser/frc_images/from\\ position\\ 1.jpg";
-	public static String image_2 = "/home/lvuser/frc_images/from\\ position\\ 2.jpg";
-	public static String image_3 = "/home/lvuser/frc_images/from\\ position\\ 3.jpg";
-	public static String image_4 = "/home/lvuser/frc_images/from\\ position\\ 4.jpg";
-	public static String image_5 = "/home/lvuser/frc_images/from\\ position\\ 5.jpg";
 	
-	Mat image, imageHSV, erode, dilate, hierarchy;
-	List<MatOfPoint> contours;
 	
-	*/
 	public CameraSubsystem(String ip){
 		//camServo = new Servo(RobotMap.Drivetrain.CAM_SERVO);
 		camPiston = new DoubleSolenoid(Drivetrain.SOLENOID_PORTS[0], Drivetrain.SOLENOID_PORTS[1], Drivetrain.SOLENOID_PORTS[2]);
@@ -96,140 +85,15 @@ public class CameraSubsystem extends Subsystem {
     		noOpencvErrors = false;
     	}
 	}
-	/*
-	public void setServoAngle(double angle){
-		camServo.setAngle(angle);
-	}
-	
-	 public Center findOneRetroTarget(Mat image){
-	    	
-	    	Center center =  new Center(-1,-1); //default
-	    	//image = new Mat();
-	    	imageHSV = new Mat();
-	    	
-	    	Imgproc.cvtColor(image, imageHSV, Imgproc.COLOR_BGR2HSV);
-	    	
-	    	//Core.inRange(imageHSV, new Scalar(78, 124, 213), new Scalar(104, 255, 255), imageHSV);
 
-	    	Core.inRange(imageHSV, new Scalar(60, 41, 218), new Scalar(94, 255, 255), imageHSV);
-	    	
-	    	//BLUR
-	    	Imgproc.GaussianBlur(imageHSV, imageHSV, new Size(5,5), 0);
-	    	
-	    	//DILATE > ERODE > DILATE 
-	    	dilate = Imgproc.getStructuringElement(Imgproc.MORPH_DILATE, new Size(3, 3));
-	    	Imgproc.dilate(imageHSV, imageHSV, dilate);//dilate  
-	    	erode = Imgproc.getStructuringElement(Imgproc.MORPH_ERODE, new Size(3, 3));
-	    	Imgproc.erode(imageHSV, imageHSV, erode);
-	    	Imgproc.dilate(imageHSV, imageHSV, dilate);
-	    	
-	    	//BLUR PART 2
-	    	Imgproc.GaussianBlur(imageHSV, imageHSV, new Size(15,15), 0);
-	    	
-	    	//THRESHING
-	    	Imgproc.threshold(imageHSV, imageHSV, 73, 255, Imgproc.THRESH_BINARY);
-	    	
-	    	//DILATE ONCE MORE
-	    	Imgproc.dilate(imageHSV, imageHSV, dilate);
-	    	
-	    	/***********
-	    	//CONTOURS AND OBJECT DETECTION
-	    	contours = new ArrayList<>();
-	    	hierarchy = new Mat();
-	    	
-	    	Moments mu;
-	    	
-	    	// find contours
-	    	Imgproc.findContours(imageHSV, contours, hierarchy, Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE);
-	    	
-	    	// if any contour exist...
-	    	if (hierarchy.size().height > 0 && hierarchy.size().width > 0)
-	    	{
-	    		int largest = 0;
-	    		
-		        // for each remaining contour, find the biggest
-		        for (int i = 0; i < contours.size(); i++)
-		        {
-		        	double area = Imgproc.contourArea(contours.get(i));
-		        	mu = Imgproc.moments(contours.get(i));
-		        	double y_coord = mu.get_m01()/mu.get_m00();
-		        	//greater than min size AND in the upper part of photo AND greater than the last biggest
-		        	if (area > 100.0  &&  y_coord < MAX_Y_COORD && area > Imgproc.contourArea(contours.get(largest))){
-		        		
-		        		largest = i;
-			        	//NetworkTable tab = NetworkTable.getTable("Obj " + i);
-		        		
-		        		//Center:    mu.m10()/mu.m00() , mu.m01()/mu.m00()
-			        	
-		  	        }
-		        }
-		        
-		        //print details of the biggest one
-	        	String name = "Obj " + largest;
-	        	Rect r = Imgproc.boundingRect(contours.get(largest));
-	        	mu = Imgproc.moments(contours.get(largest));
-	        	
-	        	//"("+ mu.get_m10()/mu.get_m00() + ", " + mu.get_m01()/mu.get_m00() + ")");
-	        	
-	        	//ASSUME LARGEST is the target, now calc dist
-	         	
-	        	double dist = calcDistAxis206(r.width, WIDTH_TARGET, 320, STANDARD_VIEW_ANGLE);
-	        	center = new Center(mu.get_m10()/mu.get_m00(), mu.get_m01()/mu.get_m00());
-	        	
-	        	String[] keys = new String[]{"Obj Center X: ", "Obj Center Y: ", "Obj Area: ", "Obj width: ", "Obj height: ", "Obj Distance: "}; 
-	        	Object[] elements = new Object[]{center.x, center.y, (Double)Imgproc.contourArea(contours.get(largest)), (Integer)r.width, (Integer)r.height, dist};
-//	        	"Obj 0 Center X: ", mu.get_m10()/mu.get_m00());
-//	        	"Obj 0 Center Y: ", mu.get_m01()/mu.get_m00());
-//	        	"Obj 0 Area: ", Imgproc.contourArea(contours.get(largest)));
-//	        	"Obj 0 width: ", r.width);
-//	        	"Obj 0 height: ", r.height);
-//	        	"Obj 0 Distance: ", dist);	
-//	        	SendableTable contourObj = new SendableTable(name, keys, elements);
-//	        	SmartDashboard.putData("Obj " + largest, contourObj);
-
-	        	
-//	        	SmartDashboard.putNumber("Obj 0 Center X: ", center.x);
-//	        	SmartDashboard.putNumber("Obj 0 Center Y: ", center.y);
-//	        	SmartDashboard.putNumber("Obj 0 Area: ", Imgproc.contourArea(contours.get(largest)));
-//	        	SmartDashboard.putNumber("Obj 0 width: ", r.width);
-//	        	SmartDashboard.putNumber("Obj 0 height: ", r.height);
-//	        	SmartDashboard.putNumber("Obj 0 Distance: ", dist);
-	        	
-	        	//Imgcodecs.imwrite("output.png", imageHSV);
-	    	}
-	    	else{
-//	    		SmartDashboard.putNumber("Obj 0 Center X: ", 0);
-//	        	SmartDashboard.putNumber("Obj 0 Center Y: ", 0);
-//	        	SmartDashboard.putNumber("Obj 0 Area: ", 0);
-//	        	SmartDashboard.putNumber("Obj 0 width: ", 0);
-//	        	SmartDashboard.putNumber("Obj 0 height: ", 0);
-//	        	SmartDashboard.putNumber("Obj 0 Distance: ", 0);
-	    	}
-	    	
-	    	
-//	    	SmartDashboard.putNumber("Number of contours in image", contours.size());
-//	    	SmartDashboard.putNumber("Mat Height", image.height());
-//	    	SmartDashboard.putNumber("Mat Width", image.width());
-	    	
-	        //mem save
-	        image.release();
-	    	imageHSV.release();
-	    	erode.release();
-	    	dilate.release();
-	    	hierarchy.release();
-	        System.gc();
-	        
-	        return center;
-    }
-	 */
 	 public void setCamera(boolean up){
 		 camPiston.set(up ? Value.kForward : Value.kReverse);
 	 }
-	 /*
-	 public double calcDistAxis206(double obj_pix, double obj_in, double view_pix, double max_cam_angle){
+	 
+	 public double calcDistAndroidCam(double obj_pix, double obj_in, double view_pix, double max_cam_angle){
     	return view_pix * obj_in / (2*Math.tan(max_cam_angle) * obj_pix);
     }
-	*/
+	
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
